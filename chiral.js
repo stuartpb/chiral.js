c9372132b1e05e3f729469c4102f156e08e4325d
Stuart P. Bentley <stuart@testtrack4.com>
2016-01-29 07:27:08 +0000

    Partial progress on a polygon-based solution

--- chiral.js
+++ chiral.js
@@ -1,4 +1,5 @@
 function chiral(opts) {
+  "use strict";
   var transformListener, startListener, endListener;
   if (typeof opts == 'function') {
     transformListener = opts;
@@ -10,12 +11,12 @@ function chiral(opts) {
 
   var points = [];
 
-  var lastAverageX;
-  var lastAverageY;
-  var thisAverageX;
-  var thisAverageY;
-  var allButAverageX;
-  var allButAverageY;
+  var lastCentroidX;
+  var lastCentroidY;
+  var thisCentroidX;
+  var thisCentroidY;
+  var lastArea;
+  var thisArea;
 
   function findPointId(id) {
     for (var i = 0; i < points.length; ++i) {
@@ -26,75 +27,268 @@ function chiral(opts) {
     return null;
   }
 
+  function angleFromFirstPoint(point) {
+    return Math.atan2(
+      point.y - points[0].y, point.x - points[0].x) + Math.PI;
+  }
+
+  function polyInsert(thisPoint) {
+    var limit = points.length - 1;
+    var inserted = false;
+    var thisAngle;
+    if (thisPoint.y < points[0].y ||
+      (thisPoint.y == points[0].y && thisPoint.x < points[0].x)) {
+
+      points.unshift(thisPoint);
+      inserted = true;
+    } else {
+      thisAngle = angleFromFirstPoint(thisPoint);
+    }
+    var point0 = points[0];
+    var point1 = points[1];
+    var x0, y0, x1, y1;
+    function shortxy() {
+      x0 = point0.x;
+      y0 = point0.y;
+      x1 = point1.x;
+      y1 = point1.y;
+    }
+    function iterate() {
+      var a = x0*y1 - x1*y0;
+      lastArea += a;
+      lastCentroidX += (x0 + x1)*a;
+      lastCentroidY += (y0 + y1)*a;
+    }
+    shortxy();
+    lastArea = x0*y1 - x1*y0;
+    lastCentroidX = (x0 + x1)*lastArea;
+    lastCentroidY = (y0 + y1)*lastArea;
+    for (var i = 1; i < limit; ++i) {
+      point0 = points[i];
+      if (!inserted) {
+        var nextAngle = angleFromFirstPoint(point0);
+        if (thisAngle < nextAngle ||
+          thisAngle == nextAngle && thisPoint.y < point0.y) {
+
+          point1 = point0;
+          point0 = thisPoint;
+          points.splice(i, 0, thisPoint);
+          inserted = true;
+          ++limit;
+        } else {
+          point1 = points[i+1];
+        }
+      } else {
+        point1 = points[i+1];
+      }
+
+      shortxy();
+      iterate();
+    }
+    if (!inserted) {
+      points[++limit] = thisPoint;
+    }
+    point0 = points[limit];
+    point1 = points[0];
+    shortxy();
+    iterate();
+    lastArea *= 0.5;
+    lastCentroidX /= (6.0*lastArea);
+    lastCentroidY /= (6.0*lastArea);
+  }
+
+  function insert(e) {
+    var thisPoint = {
+      id: e.pointerId,
+      x: e.clientX,
+      y: e.clientY
+    };
+    if (points.length == 0) {
+      points[0] = thisPoint;
+    } else if (points.length == 1) {
+      if (thisPoint.y < points[0].y ||
+        thisPoint.y == points[0].y && thisPoint.x < points[0].x) {
+
+        points[1] = points[0];
+        points[0] = thisPoint;
+      } else {
+        points[1] = thisPoint;
+      }
+
+      lastCentroidX = (points[0].x + points[1].x) / 2;
+      lastCentroidY = (points[1].y + points[0].y) / 2;
+      // for lines "area" is the square length
+      lastArea = Math.abs(points[1].x - points[0].x) +
+        (points[1].y - points[0].y); // y can't be negative because of sort
+    } else {
+      return polyInsert(thisPoint);
+    }
+  }
+
+  function polyAdjust(thisPointIndex, thisX, thisY) {
+    var i;
+    var moving = true;
+    var thisPoint = points[thisPointIndex];
+    var lastX = thisPoint.x;
+    var lastY = thisPoint.y;
+    thisPoint.x = thisX;
+    thisPoint.y = thisY;
+    var lastAngle = Math.atan2(
+      lastY - lastCentroidY, lastX - lastCentroidX);
+    var thisAngle, otherAngle;
+    if (thisY < points[0].y ||
+      (thisY == points[0].y && thisX < points[0].x)) {
+        for (i = 0; i < thisPointIndex; --i) {
+          points[i] = points[i+1];
+        }
+        points[0] = thisPoint;
+        moving = false;
+    } else {
+      thisAngle = angleFromFirstPoint(thisPoint);
+      //TODO: figure this out
+      for (i = thisPointIndex; i > 0 && moving; --i) {
+        points[i] = points[i-1];
+      }
+      points[i] = thisPoint;
+    }
+
+    thisAngle = Math.atan2(
+      thisY - thisCentroidY, thisX - thisCentroidX);
+
+    var transform = {
+      translateX: thisCentroidX - lastCentroidX,
+      translateY: thisCentroidY - lastCentroidY,
+      scale: Math.sqrt(thisArea / lastArea),
+      rotate: thisAngle - lastAngle // TODO: divide?
+    };
+
+    thisPoint.x = thisX;
+    thisPoint.y = thisY;
+    lastCentroidX = thisCentroidX;
+    lastCentroidY = thisCentroidY;
+    lastArea = thisArea;
+    return transformListener(transform);
+  }
+
+  function adjust(e, thisPointIndex) {
+    var thisX = e.clientX;
+    var thisY = e.clientY;
+    var thisPoint;
+    var transform;
+
+    if (points.length == 1) {
+      thisPoint = points[0];
+      transform = {
+        translateX: thisX - thisPoint.x,
+        translateY: thisY - thisPoint.y,
+        scale: 1,
+        rotate: 0
+      };
+      thisPoint.x = thisX;
+      thisPoint.y = thisY;
+      return transformListener(transform);
+    } else if (points.length == 2) {
+      thisPoint = points[thisPointIndex];
+      var otherPoint;
+      if (thisPointIndex == 1) {
+        otherPoint = points[0];
+        if (thisY < otherPoint.y ||
+          thisY == otherPoint.y && thisX < otherPoint.x) {
+
+          points[1] = otherPoint;
+          points[0] = thisPoint;
+        }
+      } else {
+        otherPoint = points[1];
+      }
+
+      var lastAngle = Math.atan2(
+        thisPoint.y - otherPoint.y, thisPoint.x - otherPoint.x);
+      var thisAngle = Math.atan2(
+        thisY - otherPoint.y, thisX - otherPoint.x);
+
+      thisCentroidX = (thisX + otherPoint.x) / 2;
+      thisCentroidY = (thisY + otherPoint.y) / 2;
+      thisArea = Math.abs(thisX - otherPoint.x) +
+        Math.abs(thisY - otherPoint.y);
+
+      transform = {
+        translateX: thisCentroidX - lastCentroidX,
+        translateY: thisCentroidY - lastCentroidY,
+        scale: Math.sqrt(thisArea / lastArea),
+        rotate: thisAngle - lastAngle
+      };
+
+      thisPoint.x = thisX;
+      thisPoint.y = thisY;
+      lastCentroidX = thisCentroidX;
+      lastCentroidY = thisCentroidY;
+      lastArea = thisArea;
+      return transformListener(transform);
+    } else {
+      return polyAdjust(thisPointIndex, thisX, thisY);
+    }
+  }
+
+  function polyRecalcLast() {
+    var limit = points.length - 1;
+    var point0 = points[0];
+    var point1 = points[1];
+    var x0, y0, x1, y1;
+    function shortxy() {
+      x0 = point0.x;
+      y0 = point0.y;
+      x1 = point1.x;
+      y1 = point1.y;
+    }
+    function iterate() {
+      var a = x0*y1 - x1*y0;
+      lastArea += a;
+      lastCentroidX += (x0 + x1)*a;
+      lastCentroidY += (y0 + y1)*a;
+    }
+    shortxy();
+    lastArea = x0*y1 - x1*y0;
+    lastCentroidX = (x0 + x1)*lastArea;
+    lastCentroidY = (y0 + y1)*lastArea;
+    for (var i = 1; i < limit; ++i) {
+      point0 = points[i];
+      point1 = points[i+1];
+      shortxy();
+      iterate();
+    }
+    point0 = points[limit];
+    point1 = points[0];
+    shortxy();
+    iterate();
+    lastArea *= 0.5;
+    lastCentroidX /= (6.0*lastArea);
+    lastCentroidY /= (6.0*lastArea);
+  }
+
+  function remove(index) {
+    points.splice(index, 1);
+    if (points.length > 2) {
+      return polyRecalcLast();
+    } else if (points.length == 2) {
+      lastCentroidX = Math.abs(points[1].x - points[0].x);
+      lastCentroidY = points[1].y - points[0].y; // can't be negative, sorted
+    }
+  }
+
   function listener(e) {
     var thisPointIndex;
-    var thisPoint;
-    var lastX, lastY;
-    var i, point;
     switch (e.type) {
       case 'pointerdown':
       // TODO: allow pointers to be added on enter, or move via config
-        points.push({
-          id: e.pointerId,
-          x: e.clientX,
-          y: e.clientY
-        });
-        lastAverageX = lastAverageY = 0;
-        for (i = 0; i < points.length; ++i) {
-          point = points[i];
-          lastAverageX += point.x;
-          lastAverageY += point.y;
-        }
-        lastAverageX /= points.length;
-        lastAverageY /= points.length;
-        break;
+        return insert(e);
+
       case 'pointermove':
         // if this is a drag
         if (e.buttons & 1) {
           thisPointIndex = findPointId(e.pointerId);
           if (thisPointIndex !== null) {
-            thisAverageX = thisAverageY =
-              allButAverageX = allButAverageY = 0;
-            for (i = 0; i < points.length; ++i) {
-              point = points[i];
-              if (i == thisPointIndex) {
-                thisPoint = point;
-                lastX = thisPoint.x;
-                lastY = thisPoint.y;
-                thisPoint.x = e.clientX;
-                thisPoint.y = e.clientY;
-              } else {
-                allButAverageX += point.x;
-                allButAverageY += point.y;
-              }
-              thisAverageX += point.x;
-              thisAverageY += point.y;
-            }
-            thisAverageX /= points.length;
-            thisAverageY /= points.length;
-            var transform = {
-              translateX: thisAverageX - lastAverageX,
-              translateY: thisAverageY - lastAverageY,
-              scale: 1,
-              rotate: 0
-            };
-            if (points.length > 1) {
-              allButAverageX /= points.length - 1;
-              allButAverageY /= points.length - 1;
-              transform.scale =
-                (Math.abs(thisPoint.x - allButAverageX) +
-                  Math.abs(thisPoint.y - allButAverageY)) /
-                (Math.abs(lastX - allButAverageX) +
-                  Math.abs(lastY - allButAverageY));
-              transform.rotate =
-                Math.atan2(allButAverageX - lastX,
-                  allButAverageY - lastY) -
-                Math.atan2(allButAverageX - thisPoint.x,
-                  allButAverageY - thisPoint.y);
-            }
-            lastAverageX = thisAverageX;
-            lastAverageY = thisAverageY;
-            return transformListener(transform);
+            return adjust(e, thisPointIndex);
           }
         }
         break;
@@ -103,21 +297,12 @@ function chiral(opts) {
       case 'pointerleave':
         thisPointIndex = findPointId(e.pointerId);
         if (thisPointIndex !== null) {
-          points.splice(thisPointIndex, 1);
-          if (points.length > 0) {
-            lastAverageX = lastAverageY = 0;
-            for (i = 0; i < points.length; ++i) {
-              point = points[i];
-              lastAverageX += point.x;
-              lastAverageY += point.y;
-            }
-            lastAverageX /= points.length;
-            lastAverageY /= points.length;
-          }
+          return remove(thisPointIndex);
         }
         break;
     }
   }
+
   listener.attach = function attachChiralListener(target) {
     target.addEventListener('pointerdown', listener);
     //target.addEventListener('pointerenter', listener);
